7226fee4e9b0e3600c740c9ad352789c84ef1f4f
Ayo <talktojmcvibes@gmail.com>
2024-02-13 10:34:13 +0100

    gifs

---
+++ app.js
@@ -0,0 +1,4 @@
+document.getElementById("my_frame").width = "50%";
+document.getElementById("my_frame").height = "400"
+document.getElementById("my_frame2").width = "50%";
+document.getElementById("my_frame2").height = "600";
