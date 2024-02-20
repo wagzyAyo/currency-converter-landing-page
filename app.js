2df72c08d2f3eb5beecad85fc000b021d6bcaf69
Ayo <talktojmcvibes@gmail.com>
2024-02-20 09:36:30 +0100

    remove presentatation video

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
-document.getElementById("my_frame").width = "100%";
+document.getElementById("my_frame").width = "50%";
 document.getElementById("my_frame").height = "400"
-document.getElementById("my_frame2").width = "100%";
+document.getElementById("my_frame2").width = "50%";
 document.getElementById("my_frame2").height = "600";
-document.getElementById("my_frame3").width = "100%";
-document.getElementById("my_frame3").height = "600";
